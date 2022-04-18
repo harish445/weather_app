d1a3c4cbe6ff74196dc81d83660856fc54b9e453
harish <harishthakur4455@gmail.com>
2022-04-18 19:49:24 +0530

    deploy ready
    deploy with heroku cli

--- weatherapp/settings.py
+++ weatherapp/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-wg9&w_hd+%)!w7!!pqwk_3=h2w1_2#uv(n1pechgb+m6w7xki)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['look-weather.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -124,3 +125,6 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+if os.getcwd() == '/app':
+    DEBUG = False
